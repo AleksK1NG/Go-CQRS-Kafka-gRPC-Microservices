fc099c74918e5c69229c12a19313d84959f0deae
Alexander Bryksin <alexander.bryksin@yandex.ru>
2021-08-19 13:53:57 +0300

    feature: add API Gateway Swagger

--- api_gateway_service/internal/server/http.go
+++ api_gateway_service/internal/server/http.go
@@ -1,8 +1,13 @@
 package server
 
 import (
+	"github.com/AleksK1NG/cqrs-microservices/docs"
+	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"strings"
 	"time"
+
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
 const (
@@ -11,6 +16,7 @@ const (
 	bodyLimit      = "2M"
 	readTimeout    = 15 * time.Second
 	writeTimeout   = 15 * time.Second
+	gzipLevel      = 5
 )
 
 func (s *server) runHttpServer() error {
@@ -24,6 +30,14 @@ func (s *server) runHttpServer() error {
 }
 
 func (s *server) mapRoutes() {
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Title = "API Gateway"
+	docs.SwaggerInfo.Description = "API Gateway CQRS microservices."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.BasePath = "/api/v1"
+
+	s.echo.GET("/swagger/*", echoSwagger.WrapHandler)
+
 	s.echo.Use(s.mw.RequestLoggerMiddleware)
 	s.echo.Use(middleware.RecoverWithConfig(middleware.RecoverConfig{
 		StackSize:         stackSize,
@@ -31,5 +45,11 @@ func (s *server) mapRoutes() {
 		DisableStackAll:   true,
 	}))
 	s.echo.Use(middleware.RequestID())
+	s.echo.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: gzipLevel,
+		Skipper: func(c echo.Context) bool {
+			return strings.Contains(c.Request().URL.Path, "swagger")
+		},
+	}))
 	s.echo.Use(middleware.BodyLimit(bodyLimit))
 }
